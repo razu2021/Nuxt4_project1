431461988d0c518285c6519c9f69df451e0589ed
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2025-11-16 17:55:11 +0600

    update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,8 +2,26 @@
 const { data } = await useAsyncData('test', () =>
   $fetch('https://jsonplaceholder.typicode.com/posts')
 )
+
+const users = ref({
+  name:"md razu hossain raj",
+  age:25 
+})
+
+
 </script>
 
 <template>
-  <div>{{ data }}</div>
+  <div>
+    <h1>Md Razu Hossain Raj </h1>
+      <NuxtLink to="/about"> About page</NuxtLink>
+     
+  </div>
+  
+ 
+      <h1>{{ users.name }}</h1>
+      <h1>{{ users.age }}</h1>
+
+ <NuxtImg src="/1.jpg" />
+
 </template>
