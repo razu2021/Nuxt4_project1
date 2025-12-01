a63a79f62e9762fcb1c44fc6230177a0799bab1c
Md Razu Hossain Raj <mdrazuhossainraj@gmail.com>
2025-12-01 11:32:27 +0600

    Update route-fullpath.vue

--- app/pages/route-fullpath.vue
+++ app/pages/route-fullpath.vue
@@ -27,32 +27,9 @@ const router = useRouter()
 /**========= Search Input ========= */
 const search = ref(route.query.search || "")
 const categorys = ref(route.query.categorys || "")
+const priceSort = ref("")
+const rating = ref(0)
 
-
-/**========= Update URL on typing =========
- * akhane search holo reactive value jeta input theke asce 
- * (value) value holo input box kico change hoyar pore setar update value . or search er update value last input value 
- * route.push diye amra route change korteci .. or input jeta type hobe seta abar route add hobe 
- * ... er mane holo ager joto query ace sob thakbe + search er value add hobe    ... jodi na thake tahole ager filter remove hobe 
- * search:value hocce value add hobe 
- * 
- *  */
-
-// const updateUrl = () => {
-//     router.push({
-//         query:{
-//             search: search.value || undefined,
-//             category: categorys.value || undefined,
-//         }
-//     })
-// }
-
-
-// watch([search,categorys],()=>{
-//     updateUrl()
-// })
-
-/**========= Fetch Search Result ========= */
 
 
 
@@ -66,15 +43,31 @@ const uniqueCategories = computed(()=>{
 const filteredProducts = computed(() => {
   let products = alldata.value?.products || []
 
+  /**----------- search filter --------- */
   if (search.value) {
     const q = search.value.toLowerCase()
     products = products.filter(p => p.title.toLowerCase().includes(q))
   }
 
+  /**--------- category filter ---------- */
   if (categorys.value) {
     products = products.filter(p => p.category === categorys.value)
   }
 
+
+  /**-----  price filter ------- */
+   if (priceSort.value === "low") {
+    products = [...products].sort((a, b) => a.price - b.price)
+  }
+
+  if (priceSort.value === "high") {
+    products = [...products].sort((a, b) => b.price - a.price)
+  }
+
+  /**----- Rating filter ----- */
+  if(rating.value > 0){
+    products = products.filter(p => p.rating >= rating.value)
+  }
   return products
 })
 
@@ -119,15 +112,32 @@ watch(() => route.query.search, () => {
       </div>
 
             <div class="row">
-            <div class="col-lg-3">
-      
-            <select class="form-select mb-3 form-control" aria-label="Default select example " v-model="categorys">
-            <option value="" disabled  selected>Open this select menu</option>
-            <option :value="cate" :key="cate" v-for="cate in uniqueCategories">{{ cate }}</option>
-           
-            </select>
-                
-            </div>
+              <div class="col-lg-3">
+              <select class="form-select mb-3 form-control" aria-label="Default select example " v-model="categorys">
+                <option value="" disabled  selected>Open this select menu</option>
+                <option :value="cate" :key="cate" v-for="cate in uniqueCategories">{{ cate }}</option>
+              </select>
+              </div>
+              <!-- col end  -->
+              <div class="col-lg-3">
+              <select class="form-select mb-3 form-control" aria-label="Default select example " v-model="priceSort">
+                <option value="" disabled  selected>Select Price </option>
+                <option value="low" >Low to High</option>
+                <option value="high" > High to Low</option>
+              </select>
+              </div>
+              <!-- col end  -->
+              <div class="col-lg-3">
+                <select v-model="rating" class="border p-2 form-control form-select">
+                  <option value="0" selected disabled>All Ratings</option>
+                  <option value="1">1★ & Up</option>
+                  <option value="2">2★ & Up</option>
+                  <option value="3">3★ & Up</option>
+                  <option value="4">4★ & Up</option>
+                  <option value="5">5★ Only</option>
+                </select>
+              </div>
+              <!-- col end  -->
             </div>
 
       <div class="row">
@@ -136,6 +146,7 @@ watch(() => route.query.search, () => {
           class="col-12 col-sm-6 col-md-6 col-lg-3 mb-3" 
           v-for="pro in filteredProducts" :key="pro.id" >
           <div class="card" style="width: 18rem;">
+             <img :src="pro.thumbnail" class="card-img-top" alt="dfdf">
             <div class="card-body">
               <h5 class="card-title">{{ pro.title }}</h5>
               <h6 class="card-subtitle mb-2 text-body-secondary">
@@ -143,6 +154,7 @@ watch(() => route.query.search, () => {
               </h6>
               <p class="card-text">{{ pro.description }}</p>
               <h5>{{ pro.category }}</h5>
+              <p><span class="text-warning"> Rating: {{ pro.rating }}</span></p>
               <a href="#" class="card-link">More Details</a>
             </div>
           </div>
